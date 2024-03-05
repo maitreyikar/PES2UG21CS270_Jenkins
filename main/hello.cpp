8bb0392e91a9a59d0469ed6a532de5eb4c16a1c1
Maitreyi <maitreyikar13@gmail.com>
2024-03-05 21:21:51 +0530

    modified hello.cpp for task 4 again

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "Making changes for lab 4, task 4\n";
+  cout << "Web hook successfully installed\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
